3a06530b371ab0c2bc6068ae3d83451f69bf1fd5
Kylor Hall <kylor@sharesight.com>
2018-04-03 13:15:05 +1200

    Remove last_updated_at.

--- mappers/help/page.rb
+++ mappers/help/page.rb
@@ -3,11 +3,10 @@
 class HelpPageMapper < ContentfulMiddleman::Mapper::Base
   def map(context, entry)
     super
-    keys = entry.fields_with_locales.keys
 
-    if keys.include?(:updatedAt)
-      context.last_updated_at = map_locales(context.updatedAt){ |value| value.strftime("%B %-d, %Y") } rescue entry.name
-    end
+    # does nothing
+
+    return context
   end
 
   def map_locales(entry, &block)
