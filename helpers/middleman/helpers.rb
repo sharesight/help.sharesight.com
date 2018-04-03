fbe632dcf96fa5f071ec32bb0dad7d73ac90d517
Kylor Hall <kylor@sharesight.com>
2018-04-03 12:40:49 +1200

    Require the correct files.

--- helpers/middleman/helpers.rb
+++ helpers/middleman/helpers.rb
@@ -1,12 +1,13 @@
 require 'ostruct'
-load File::expand_path('../basic_helper.rb', __dir__)
 
 # Load these as they're prone to change.
+load File::expand_path('./collection.rb', __dir__)
 load File::expand_path('./locale.rb', __dir__)
 load File::expand_path('./page.rb', __dir__)
 load File::expand_path('./url.rb', __dir__)
 
 module MiddlemanHelpers
+  include ::MiddlemanCollectionHelpers
   include ::MiddlemanLocaleHelpers
   include ::MiddlemanPageHelpers
   include ::MiddlemanUrlHelpers
