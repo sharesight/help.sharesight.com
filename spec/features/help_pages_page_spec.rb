85df5912e119606c4478043abc2d437c07cd1b44
Kylor Hall <kylor@sharesight.com>
2018-04-05 14:14:51 +1200

    Test most things on generated help pages.

--- spec/features/help_pages_page_spec.rb
+++ spec/features/help_pages_page_spec.rb
@@ -1,44 +1,36 @@
 require 'cgi'
 require 'spec_helper'
 
-describe 'Partners Partner Pages', :type => :feature do
-
-  it "should load" do
+describe 'Help Pages', :type => :feature do
+  it "should load and have expected base metas" do
     locales.each do |locale|
-      get_partners_partners(locale).each do |partner|
-        visit partner[:path]
+      get_pages(locale, true).each do |_page|
+        visit _page[:path]
 
         expect(page).to respond_successfully
-      end
-    end
-  end
-
-  it "should have expected base metas" do
-    locales.each do |locale|
-      get_partners_partners(locale).each do |partner|
-        visit partner[:path]
 
         expect(page).to have_base_metas()
         expect(page).to have_social_metas()
-        expect(page).to have_titles(partner[:page_title])
-        expect(page).to have_descriptions(partner[:meta_description])
+        expect(page).to have_titles(_page[:page_title])
+        expect(page).to have_descriptions(_page[:meta_description])
       end
     end
   end
 
   it "should have expected urls" do
     locales.each do |locale|
-      get_partners_partners(locale).each do |partner|
-        visit partner[:path]
+      get_pages(locale, true).each do |_page|
+        visit _page[:path]
 
-        expect(page).to have_meta('og:url', base_url(partner[:path]), name_key: 'property')
+        expect(page).to have_meta('og:url', base_url(_page[:path]), name_key: 'property')
 
-        expect(page).to have_head('link', args: { rel: 'canonical', href: absolute_url(partner[:path]) }, debug: :href)
-        locales.each do |alternate_locale|
+        expect(page).to have_head('link', args: { rel: 'canonical', href: absolute_url(_page[:path]) }, debug: :href)
+
+        Capybara.app.page_content_locales(_page).each do |alternate_locale|
           if alternate_locale[:id] == default_locale_id
-            expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(partner[:path], locale_id: default_locale_id), hreflang: 'x-default' }, debug: :href)
+            expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(_page[:path], locale_id: default_locale_id), hreflang: 'x-default' }, debug: :href)
           end
-          expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(partner[:path], locale_id: alternate_locale[:id]), hreflang: alternate_locale[:lang].downcase }, debug: :href)
+          expect(page).to have_head('link', args: { rel: 'alternate', href: localize_url(_page[:path], locale_id: alternate_locale[:id]), hreflang: alternate_locale[:lang].downcase }, debug: :href)
         end
       end
     end
@@ -46,32 +38,65 @@ describe 'Partners Partner Pages', :type => :feature do
 
   it "should have the expected elements" do
     locales.each do |locale|
-      get_partners_partners(locale).each do |partner|
-        visit partner[:path]
+      get_pages(locale, true).each do |_page|
+        visit _page[:path]
 
-        expect(page).to have_img(partner[:logo][:url]) if partner[:logo]
-        expect(page).to have_css('.partner__title', text: /#{Regexp.escape(partner[:name].strip)}$/) if partner[:name]
+        expect(page).to have_css('.breadcrumbs li', text: "Help")
+        expect(page).to have_css('.breadcrumbs li', text: _page[:parent_page][:title].strip) if _page[:parent_page]
+        expect(page).to have_css('.breadcrumbs li.breadcrumb-extra', text: _page[:title].strip)
 
-        if partner[:featured_link] && partner[:featured_link][:link]
-          expect(page).to have_css('.featured_link__description', text: partner[:featured_link][:description]) if partner[:featured_link][:description]
-          expect(page).to have_css('a.featured_link__button', text: partner[:featured_link][:button_text]) if partner[:featured_link][:button_text]
+        expect(page).to have_css('h1', text: _page[:title].strip)
+        expect(page).to have_css('p.date_modified', text: /Last modified on/)
+      end
+    end
+  end
+
+  it "should have all help topics in navigation" do
+    locales.each do |locale|
+      get_pages(locale, true).each do |_page|
+        visit _page[:path]
+
+        get_categories(locale).each do |category|
+          expect(page).to have_css("h4[data-toggle]", text: category[:name].strip), "Could not find #{category[:name]} in navigation on #{_page[:path]}"
         end
 
-        # Fairly dumb checks for content.
-        expect(page).to have_css('.partner__details dd', text: /#{Regexp.escape(partner[:address].strip)}/) if partner[:address]
-        expect(page).to have_css('.partner__details dd', text: /#{Regexp.escape(partner[:city].strip)}/) if partner[:address] && partner[:city]
-        expect(page).to have_css('.partner__details dd', text: /#{Regexp.escape(partner[:state].strip)}/) if partner[:address] && partner[:state]
-        expect(page).to have_css('.partner__details dd', text: /#{Regexp.escape(partner[:email].strip)}/) if partner[:email]
-        expect(page).to have_css('.partner__details dd', text: /#{Regexp.escape(partner[:phone_number].strip)}/) if partner[:phone_number]
+        pages_in_this_locale = get_pages(locale)
 
-        # Dumb checks for just titles.
-        expect(page).to have_css('.partner__details dt', text: 'Website') if partner[:website]
-        expect(page).to have_css('.partner__details dt', text: 'Category') if partner[:categories].length == 1
-        expect(page).to have_css('.partner__details dt', text: 'Categories') if partner[:categories].length > 1
-
-        # Have a footer.
-        expect(page).to have_css('.footer__copyright', text: 'All rights reserved.')
+        pages_in_this_locale.each do |_page|
+          # if the page has multiple parents, it doesn't show; navigation only goes one layer deep
+          has_multiple_parents = true if _page[:parent_page] && _page[:parent_page][:parent_page]
+          unless has_multiple_parents
+            page_url = Capybara.app.page_url(_page, locale_id: locale[:id])
+            expect(page).to have_css("a[href='#{page_url}']", text: _page[:title].strip, visible: false) # visible or not
+          end
+        end
       end
+    end
+  end
+
+  it "should have a search field" do
+    locales.each do |locale|
+      visit locale.path
+
+      expect(page).to have_css('input#search-field')
+    end
+  end
+
+  it "should have login and go to sharesight buttons" do
+    locales.each do |locale|
+      visit locale.path
+
+      expect(page).to have_css('a', text: 'Login')
+      expect(page).to have_css('a', text: 'Go to sharesight.com')
+    end
+  end
+
+  it "should have a footer" do
+    locales.each do |locale|
+      visit locale.path
+
+      expect(page).to have_css('p.footer__intro', text: 'Simply the best portfolio management tool for DIY investors.')
+      expect(page).to have_css('.footer__copyright', text: 'All rights reserved.')
     end
   end
 end
