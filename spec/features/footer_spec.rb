bb33d6b9f7c6d4e2624dbb224b2370d8abc6ad99
Iain Walker <iain.walker@sharesight.co.nz>
2020-09-04 09:06:37 +1200

    Add reviews link to footer and footers spec

---
+++ spec/features/footer_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'Footer', type: :feature do
+  it 'should have the correct links' do
+    [
+      /^www.sharesight.com/,
+      /^About Us/,
+      /^Pricing/,
+      /^Reviews/,
+      /^FAQ/,
+      /^Blog/,
+      /^Partners/,
+      /^Developer API/,
+      /^Privacy Policy/,
+      /^Terms of Use/,
+      /^Pro Terms of Use/,
+    ].each do |text|
+      visit '/'
+      expect(page.find('a.footer__link', text: text)).to_not be_nil
+    end
+  end
+end
