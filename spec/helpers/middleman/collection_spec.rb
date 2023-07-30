24f0efd7e65297090418f3bd770164a045200f16
Maisie <maisie.embleton@sharesight.co.nz>
2023-07-30 15:57:16 +0800

    Fixing up new_content_url test

--- spec/helpers/middleman/collection_spec.rb
+++ spec/helpers/middleman/collection_spec.rb
@@ -42,7 +42,7 @@ describe 'Collection Middleman Helper', :type => :helper do
       get_pages().each do |_page|
         _page.static_url_slug = nil
         expect(@app.new_content_url(_page)).to be_kind_of(::String)
-        expect(@app.new_content_url(_page)).to include("http://localhost:4567/")
+        expect(@app.new_content_url(_page)).to include("http://" || 'https://')
       end
     end
 
